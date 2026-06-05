3d9763627f246a128876b3d97bd41c2513c8e900
Nguyen Van Minh <nvminh162@gmail.com>
2026-06-05 16:09:59 +0700

    feat: update CI/CD pipeline to deploy production branch and ensure service restart policy

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -1,9 +1,9 @@
 cd /home/ubuntu/aurora-hotel-management-system/aurora-backend || exit
 
-echo "Deploying branch master..."
+echo "Deploying branch production..."
 git fetch -a
-git checkout master
-git pull origin master
+git checkout production
+git pull origin production
 
 docker compose up -d --pull always
 docker system prune -af
